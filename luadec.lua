d5ed1d8e392343ea4f6b1b7a1d539e3825481b83
Yue-bin <yuebinjiang@qq.com>
2025-03-06 22:54:53 +0800

    🐞 fix: 一些问题
    1.多个装饰器同时装饰的时候只会有一个生效
    2.非可调用对象会导致一些很奇怪的问题

--- luadec.lua
+++ luadec.lua
@@ -10,22 +10,31 @@ function _M.add_decorator(name, decorator)
     status[name] = false
 end
 
-local function decorate(func, event)
+local function decorate(callable, event)
     return function()
-        _M.decorators[event](func)
+        _M.decorators[event](callable)
     end
+end
+
+local function is_callable(obj)
+    return base.type(obj) == "function" or (base.getmetatable(obj) and base.getmetatable(obj).__call)
 end
 
 function _M.start_listen(table_to_listen)
     base.setmetatable(table_to_listen, {
         __newindex = function(t, k, v)
+            if not is_callable(v) then
+                rawset(t, k, v)
+                return
+            end
             for name, stat in pairs(status) do
                 if stat then
-                    rawset(t, k, decorate(v, name))
+                    v = decorate(v, name)
                     --t[k] = decorate(v, name)
                     status[name] = false
                 end
             end
+            rawset(t, k, v)
         end
     })
 end
