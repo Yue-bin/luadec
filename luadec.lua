10c56b04ed1e507b64267bbe0673e5221f51765f
Yue-bin <yuebinjiang@qq.com>
2025-03-06 23:56:25 +0800

    ✨ feat: 大修
    1.取消暴露decorators
    2.删除了status
    3.删除了safemode
    4.在at中加入了assert以判断装饰器是否存在

--- luadec.lua
+++ luadec.lua
@@ -1,34 +1,17 @@
 local base = _G
 local _M = {}
 
-_M.decorators = {}
+local decorators = {}
 
-local status = {}
-
-local function decorate(callable, event)
+local function decorate(callable, decorator)
     return function()
-        _M.decorators[event](callable)
+        decorator(callable)
     end
 end
 
 local function is_callable(obj)
     return base.type(obj) == "function" or (base.getmetatable(obj) and base.getmetatable(obj).__call)
 end
-
-local function has_positional_arg(func, position)
-    if base.type(func) == "table" then
-        local mt = base.getmetatable(func)
-        -- 前面已经保证func是可调用的
-        return has_positional_arg(mt.__call, position)
-    end
-
-    local info = base.debug.getinfo(func, "u")
-    local nparams = base.math.abs(info.nparams) -- 处理可变参数（负值表示固定参数数量）
-
-    return position <= nparams
-end
-
-_M.safe_mode = false
 
 function _M.start_listen(table_to_listen)
     base.setmetatable(table_to_listen, {
@@ -37,11 +20,11 @@ function _M.start_listen(table_to_listen)
                 base.rawset(t, k, v)
                 return
             end
-            for name, stat in base.pairs(status) do
-                if stat then
-                    v = decorate(v, name)
+            for name, decorator in base.pairs(decorators) do
+                if decorator.status then
+                    v = decorate(v, decorator.func)
                     --t[k] = decorate(v, name)
-                    status[name] = false
+                    decorators[name].status = false
                 end
             end
             base.rawset(t, k, v)
@@ -51,15 +34,15 @@ end
 
 function _M.add_decorator(name, decorator)
     base.assert(is_callable(decorate), "Decorator already exists")
-    if _M.safe_mode then
-        base.assert(has_positional_arg(decorator, 1), "Decorator must have at least one argument")
-    end
-    _M.decorators[name] = decorator
-    status[name] = false
+    decorators[name] = {
+        func = decorator,
+        status = false
+    }
 end
 
 function _M.at(name)
-    status[name] = true
+    base.assert(decorators[name], "Decorator not found")
+    decorators[name].status = true
 end
 
 return _M
